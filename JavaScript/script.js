1c0372c146ff663f17c6b878a3a0b123575662a7
FabSpore01 <135708563+FabSpore01@users.noreply.github.com>
2023-08-10 10:48:17 -0300

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,11 @@
+function galletas(){
+    if (confirm("¿De verdad queres una galletita?")) {
+        alert("Aqui tenes tus galletitas")
+        prompt("¿Y Estuvieron ricas?")
+    }
+    
+    else{
+        alert("ah sos bigote")
+        alert("Bueno, pasa igual")
+    }
+}
