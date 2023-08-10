4e955502c1fecca63c209970c5f6917a67f9f097
Lucas Zarandón <86167490+lucaszhh@users.noreply.github.com>
2023-08-10 12:33:20 -0300

    Add files via upload

---
+++ 12_javascript-galletas/script.js
@@ -0,0 +1,19 @@
+function quieresGalletas() {
+    if (confirm("¿Quíeres galletas?")) {
+        // Esto se ejecuta si es TRUE
+        alert("Aqui tienes tus galletas")
+        prompt("¿Cómo estubieron las galletas?")
+    } 
+    else{   
+        // Esto se ejecuta si es FALSE 
+        alert("Lastima mas para mi")
+    }
+}
+
+
+
+
+
+
+
+
