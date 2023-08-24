1ed7c4db20b70f2902b7eb65162f676cc4da2f05
JeroAsenjo <135977816+JeroAsenjo@users.noreply.github.com>
2023-08-24 11:03:11 -0300

    Add files via upload

--- 15_comentarios/script.js
+++ 15_comentarios/script.js
@@ -12,4 +12,5 @@ function comentar() {
     /* capturar valor del input */
     /* Agregar un comentario al div con clase de comentarios  */
     comentarios.innerHTML += `<p>${input.value}</p>`
+    
 }
