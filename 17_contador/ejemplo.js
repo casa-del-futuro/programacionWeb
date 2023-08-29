d23f0eef0f903c09de46a953f6fdd49d37277a93
Lucas Zarandón <86167490+lucaszhh@users.noreply.github.com>
2023-08-29 10:58:30 -0300

    add contador

---
+++ 17_contador/ejemplo.js
@@ -0,0 +1,12 @@
+// ¿qué es una variable?
+// lugar en la memoria temporal que nos permite almacenar un valor
+
+// ¿qué variables hay?
+var nombre = "525lucas" // tipo de dato alfanumerico
+var esVerano = true || false // booleano
+var numero = -23.2 // numerico 
+var listadoNumeros = ["uno", 2, "tres"] // listas
+var persona = {nombres: ["lucas", "martin"], edad: 24,  } // objeto
+
+// ¿para qué sirve?
+// nos permite almacenar un valor
